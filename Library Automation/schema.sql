01f8cb52f6bfa20fef5e00f5758bf5505638ba42
Fuat Şimşek <161131999+fuatsimsek@users.noreply.github.com>
2025-02-21 21:07:11 +0300

    Update schema.sql

--- Library Automation/schema.sql
+++ Library Automation/schema.sql
@@ -1,68 +1,166 @@
-CREATE TABLE AppRoles (
-    RoleId INT IDENTITY(1,1) PRIMARY KEY,
-    RoleName NVARCHAR(100) NOT NULL
-);
-
-CREATE TABLE AppUsers (
-    UserId INT IDENTITY(1,1) PRIMARY KEY,
-    FirstName NVARCHAR(100) NOT NULL,
-    LastName NVARCHAR(100) NOT NULL,
-    IdentityNumber NVARCHAR(50) UNIQUE NOT NULL,
-    Username NVARCHAR(50) UNIQUE NOT NULL,
-    Password NVARCHAR(255) NOT NULL,
-    BirthDate DATE NOT NULL,
-    Gender NVARCHAR(10) CHECK (Gender IN ('Male', 'Female', 'Other')),
-    ModifiedBy INT NULL,
-    CreatedDate DATETIME DEFAULT GETDATE(),
-    Status BIT DEFAULT 1,
-    Balance DECIMAL(10,2) DEFAULT 0.00
-);
-
-CREATE TABLE UserRoles (
-    RoleId INT NOT NULL,
-    UserId INT NOT NULL,
-    CreatedDate DATETIME DEFAULT GETDATE(),
-    Status BIT DEFAULT 1,
-    PRIMARY KEY (RoleId, UserId),
-    FOREIGN KEY (RoleId) REFERENCES AppRoles(RoleId),
-    FOREIGN KEY (UserId) REFERENCES AppUsers(UserId)
-);
-
-CREATE TABLE Developers (
-    DeveloperId INT IDENTITY(1,1) PRIMARY KEY,
-    FirstName NVARCHAR(100) NOT NULL,
-    LastName NVARCHAR(100) NOT NULL,
-    CreatedDate DATETIME DEFAULT GETDATE()
-);
-
-CREATE TABLE Games (
-    GameId INT IDENTITY(1,1) PRIMARY KEY,
-    GameName NVARCHAR(255) NOT NULL,
-    PublisherName NVARCHAR(255) NOT NULL,
-    ReleasedDate DATE NOT NULL,
-    NumberOfEpisodes INT DEFAULT 0,
-    Stocks INT DEFAULT 0,
-    CreatedDate DATETIME DEFAULT GETDATE(),
-    Status BIT DEFAULT 1,
-    ModifiedDate DATETIME NULL,
-    CreatedBy INT NULL,
-    GameImg NVARCHAR(255) NULL,
-    Price DECIMAL(10,2) DEFAULT 0.00,
-    FOREIGN KEY (CreatedBy) REFERENCES AppUsers(UserId)
-);
-
-CREATE TABLE GameDevelopers (
-    GameId INT NOT NULL,
-    DeveloperId INT NOT NULL,
-    PRIMARY KEY (GameId, DeveloperId),
-    FOREIGN KEY (GameId) REFERENCES Games(GameId),
-    FOREIGN KEY (DeveloperId) REFERENCES Developers(DeveloperId)
-);
-
-CREATE TABLE UserGamesOwned (
-    UserId INT NOT NULL,
-    GameId INT NOT NULL,
-    PRIMARY KEY (UserId, GameId),
-    FOREIGN KEY (UserId) REFERENCES AppUsers(UserId),
-    FOREIGN KEY (GameId) REFERENCES Games(GameId)
-);
+USE [myGameSalesAutomation]
+GO
+/****** Object:  Table [dbo].[AppRoles]    Script Date: 21.02.2025 20:59:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AppRoles](
+	[RoleId] [int] IDENTITY(1,1) NOT NULL,
+	[RoleName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_AppRoles] PRIMARY KEY CLUSTERED 
+(
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AppUsers]    Script Date: 21.02.2025 20:59:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AppUsers](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](100) NULL,
+	[LastName] [nvarchar](100) NULL,
+	[IdentityNumber] [nvarchar](20) NULL,
+	[Username] [nvarchar](100) NULL,
+	[Password] [nvarchar](100) NULL,
+	[BirthDate] [date] NULL,
+	[Gender] [bit] NULL,
+	[ModifiedBy] [int] NULL,
+	[CreatedDate] [datetime] NULL,
+	[Status] [bit] NULL,
+	[Balance] [int] NULL,
+ CONSTRAINT [PK_AppUsers] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Developers]    Script Date: 21.02.2025 20:59:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Developers](
+	[DeveloperId] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](150) NULL,
+	[LastName] [nvarchar](150) NULL,
+	[CreatedDate] [datetime] NULL,
+	[Status] [bit] NULL,
+ CONSTRAINT [PK_Developer] PRIMARY KEY CLUSTERED 
+(
+	[DeveloperId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[GameDevelopers]    Script Date: 21.02.2025 20:59:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[GameDevelopers](
+	[GameId] [int] NOT NULL,
+	[DeveloperId] [int] NOT NULL,
+ CONSTRAINT [PK_GameDevelopers] PRIMARY KEY CLUSTERED 
+(
+	[GameId] ASC,
+	[DeveloperId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Games]    Script Date: 21.02.2025 20:59:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Games](
+	[GameId] [int] IDENTITY(1,1) NOT NULL,
+	[GameName] [nvarchar](250) NULL,
+	[PublisherName] [nvarchar](250) NULL,
+	[ReleasedDate] [date] NULL,
+	[NumberOfEpisodes] [int] NULL,
+	[Stocks] [int] NULL,
+	[CreatedDate] [datetime] NULL,
+	[Status] [bit] NULL,
+	[ModifiedDate] [datetime] NULL,
+	[CreatedBy] [int] NULL,
+	[GameImg] [nvarchar](500) NULL,
+	[Price] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Games] PRIMARY KEY CLUSTERED 
+(
+	[GameId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserGamesOwned]    Script Date: 21.02.2025 20:59:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserGamesOwned](
+	[UserId] [int] NOT NULL,
+	[GameId] [int] NOT NULL,
+ CONSTRAINT [PK_UserGamesOwned] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[GameId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserRoles]    Script Date: 21.02.2025 20:59:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserRoles](
+	[RoleId] [int] NOT NULL,
+	[UserId] [int] NOT NULL,
+	[CreatedDate] [datetime] NULL,
+	[Status] [bit] NULL,
+ CONSTRAINT [PK_UserRoles] PRIMARY KEY CLUSTERED 
+(
+	[RoleId] ASC,
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AppUsers] ADD  CONSTRAINT [DF_AppUsers_CreatedDate]  DEFAULT (getdate()) FOR [CreatedDate]
+GO
+ALTER TABLE [dbo].[Developers] ADD  CONSTRAINT [DF_Developer_CreatedDate]  DEFAULT (getdate()) FOR [CreatedDate]
+GO
+ALTER TABLE [dbo].[Games] ADD  CONSTRAINT [DF_Games_CreatedDate]  DEFAULT (getdate()) FOR [CreatedDate]
+GO
+ALTER TABLE [dbo].[UserRoles] ADD  CONSTRAINT [DF_UserRoles_CreatedDate]  DEFAULT (getdate()) FOR [CreatedDate]
+GO
+ALTER TABLE [dbo].[GameDevelopers]  WITH CHECK ADD  CONSTRAINT [FK_GameDevelopers_Developers] FOREIGN KEY([DeveloperId])
+REFERENCES [dbo].[Developers] ([DeveloperId])
+GO
+ALTER TABLE [dbo].[GameDevelopers] CHECK CONSTRAINT [FK_GameDevelopers_Developers]
+GO
+ALTER TABLE [dbo].[GameDevelopers]  WITH CHECK ADD  CONSTRAINT [FK_GameDevelopers_Games1] FOREIGN KEY([GameId])
+REFERENCES [dbo].[Games] ([GameId])
+GO
+ALTER TABLE [dbo].[GameDevelopers] CHECK CONSTRAINT [FK_GameDevelopers_Games1]
+GO
+ALTER TABLE [dbo].[UserGamesOwned]  WITH CHECK ADD  CONSTRAINT [FK_UserGamesOwned_AppUsers] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AppUsers] ([UserId])
+GO
+ALTER TABLE [dbo].[UserGamesOwned] CHECK CONSTRAINT [FK_UserGamesOwned_AppUsers]
+GO
+ALTER TABLE [dbo].[UserGamesOwned]  WITH CHECK ADD  CONSTRAINT [FK_UserGamesOwned_Games] FOREIGN KEY([GameId])
+REFERENCES [dbo].[Games] ([GameId])
+GO
+ALTER TABLE [dbo].[UserGamesOwned] CHECK CONSTRAINT [FK_UserGamesOwned_Games]
+GO
+ALTER TABLE [dbo].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_UserRoles_AppRoles1] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[AppRoles] ([RoleId])
+GO
+ALTER TABLE [dbo].[UserRoles] CHECK CONSTRAINT [FK_UserRoles_AppRoles1]
+GO
+ALTER TABLE [dbo].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_UserRoles_AppUsers1] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AppUsers] ([UserId])
+GO
+ALTER TABLE [dbo].[UserRoles] CHECK CONSTRAINT [FK_UserRoles_AppUsers1]
+GO
